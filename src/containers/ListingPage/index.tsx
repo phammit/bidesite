0e7a350e938a1a28e92c23d898e5b89d3a33b499
minh pham <phammit@gmail.com>
2021-02-21 22:00:47 -0600

    + carousel, may implement something else

--- src/containers/ListingPage/index.tsx
+++ src/containers/ListingPage/index.tsx
@@ -5,6 +5,8 @@ import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
 
 
 interface someProps {
@@ -18,10 +20,19 @@ function handleClick(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
 
 const useStyles = makeStyles({
     breadcrumb: {
-        marginLeft: '20px',
+        marginLeft: '40px',
     },
     container: {
         border: '1px',
+    },
+    addpadding: {
+        padding: "20px",
+    },
+    formargin: {
+        margin: "0px 30px",
+    },
+    rightcolumn: {
+        //marginLeft: "10px",
     },
 })
 
@@ -46,20 +57,21 @@ const ListingPage: FC<someProps> = ({}) => {
                     </Grid>
                 </Grid>
             </Grid>
-            <Grid item xs={12}>
-                <Grid container className={classes.container}>
-                    <Grid item xs={8} className={classes.container}>
+            <Grid item xs={12} className={classes.formargin}>
+                <Grid container className={classes.container} spacing={3}>
+                    <Grid item xs={7} className={classes.container}>
                         <Paper>Product Column Section 1</Paper>
                         <Grid container>
-                            <Grid item xs={12}>
+                            <Grid item xs={12} className={classes.addpadding}>
                                 <Paper>Pictures of product with carousol</Paper>
+                                <ImageGallery items={images} showPlayButton={false} showBullets={true}/>
                             </Grid>
                             <Grid item xs={12}>
                                 <Paper>HTML editor</Paper>
                             </Grid>
                         </Grid>
                     </Grid>
-                    <Grid item xs={4}>
+                    <Grid item xs={4} className={classes.rightcolumn}>
                         <Paper>Item Information Column</Paper>
                     </Grid>
                 </Grid>
@@ -70,3 +82,30 @@ const ListingPage: FC<someProps> = ({}) => {
 }
 
 export default ListingPage;
+
+const images = [
+    {
+        original: "http://localhost:3000/images/miniMiner.jpg",
+        thumbnail: "http://localhost:3000/images/miniMiner.jpg",
+    },
+    {
+        original: "http://localhost:3000/images/bitcoin.jpg",
+        thumbnail: "http://localhost:3000/images/bitcoin.jpg",
+    },
+    {
+        original: "http://localhost:3000/images/coolGraphicsCard.jpg",
+        thumbnail: "http://localhost:3000/images/coolGraphicsCard.jpg",
+    },
+    {
+        original: "http://localhost:3000/images/miniWallet.jpg",
+        thumbnail: "http://localhost:3000/images/miniWallet.jpg",
+    },
+    {
+        original: "http://localhost:3000/images/redScooter.jpg",
+        thumbnail: "http://localhost:3000/images/redScooter.jpg",
+    },
+    {
+        original: "http://localhost:3000/images/RTX2080.jpg",
+        thumbnail: "http://localhost:3000/images/RTX2080.jpg",
+    },
+]
